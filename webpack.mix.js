70916516674a26b53331c5384654d03455b001be
syau <syausyauu@gmail.com>
2025-11-09 01:41:45 +0700

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+mix.js('resources/js/order/shared.js', 'public/js/orders')
+   .js('resources/js/order/cart.js', 'public/js/orders')
+   .js('resources/js/order/customer.js', 'public/js/orders')
+   .js('resources/js/order/payment.js', 'public/js/orders')
+   .js('resources/js/order/status.js', 'public/js/orders')
+   .css('resources/css/order.css', 'public/css');
